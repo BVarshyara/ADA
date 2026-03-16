9d3d5e6c1fc564e67f1cdecdd825dc987480f50c
Varshyara Bukkapatnam <varshyara.cs24@bmsce.ac.in>
2026-03-16 15:52:01 +0530

    Add files via upload

---
+++ 16-03-2026/gcd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int gcd(int a, int b)
+{
+    int r;
+    while(b != 0)
+    {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+int main()
+{
+    int a, b, result;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    result = gcd(a, b);
+    printf("GCD of %d and %d is %d\n", a, b, result);
+    return 0;
+}
